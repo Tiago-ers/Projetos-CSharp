d439ba22824e9c5a1d7b6de1076f432351a99610
Tiago-ers <trsilva284@gmail.com>
2021-01-19 11:21:24 -0300

    produos

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        // Método to string com sobreposição
+        public override string ToString()
+        {
+            return "Produto " + Nome + ", Valor unitário $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade 
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
